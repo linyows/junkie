75f1c00672c7ca38095905ef056dafe4f1d71b6c
linyows <linyows@gmail.com>
2019-08-17 22:35:24 +0900

    add api for github

--- src/github.ts
+++ src/github.ts
@@ -26,18 +26,64 @@ export class Github {
     }
   }
 
-  public reposByUserAndLang(u string, l string) {
-    return this.searchRepos(`+user:${u}+languages:${l}`)
+  public reposByUserAndLang(u: string, l: string) {
+    const res = this.searchRepos(`+language:${l}+user:${u}`)
+    return (res.items.length > 0) ? res.items : []
   }
 
-  public searchRepos(q string) {
+  public searchRepos(q: string) {
     const defaultUrl = `${this.apiEndpoint}search/repositories?q=${q}&per_page=100`
-    const optionUrl = opts ? Github.buildOptionUrl(opts) : ''
-    const res = UrlFetchApp.fetch(`${defaultUrl}${optionUrl}`, {
+    const res = UrlFetchApp.fetch(`${defaultUrl}`, {
       method: 'get',
       headers: this.headers
     })
 
     return JSON.parse(res.getContentText())
+  }
+
+  public repoHooks(repo: string) {
+    const defaultUrl = `${this.apiEndpoint}repos/${repo}/hooks`
+    const res = UrlFetchApp.fetch(`${defaultUrl}`, {
+      method: 'get',
+      headers: this.headers
+    })
+
+    return JSON.parse(res.getContentText())
+  }
+
+  public findHook(repo: string, webhook: string) {
+    const hooks = this.repoHooks(repo)
+    for (const h of hooks) {
+      if (h.active && h.config.url === webhook) {
+        return h
+      }
+    }
+  }
+
+  public createHook(repo: string, webhook: string, events: string[]) {
+    console.log(repo + ' : create hook!!!!!!!!!!!!!!!!!!!!!!!!')
+    // const res = UrlFetchApp.fetch(`${this.apiEndpoint}/repos/${repo}/hooks`, {
+    //   method: 'post',
+    //   headers: this.headers,
+    //   payload: {
+    //     config: { url: webhook },
+    //     events: events
+    //   }
+    // })
+
+    // return JSON.parse(res.getContentText())
+  }
+
+  public updateHookEvents(repo: string, id: number, events: string[]) {
+    console.log(repo + ' : update hook-----------------------')
+    // const res = UrlFetchApp.fetch(`${this.apiEndpoint}/repos/${repo}/hooks/${id}`, {
+    //   method: 'post',
+    //   headers: this.headers,
+    //   payload: {
+    //     events: events
+    //   }
+    // })
+
+    // return JSON.parse(res.getContentText())
   }
 }
